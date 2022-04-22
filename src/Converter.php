68bf96c8ea75ac929bc82dee74073fa6c1476444
Niki <82795329+NikiFutaki@users.noreply.github.com>
2022-04-22 13:49:07 +0900

    Create Converter

---
+++ src/Converter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Nikifutaki\AlphabetNumerals;
+
+class Converter
+{
+  static protected $alphabet = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ];
+
+  public static function encode(int $number)
+  {
+    if(!preg_match('/[0-9]*/u',$number))return false;
+    if(!$number)return false;
+
+    $result = '';
+    $count = 0;
+      
+    while($number)
+    {
+      $mod = ($number-1)%26;
+      $result = self::$alphabet[$mod].$result;
+      $number = ($number-$mod-1)/26;
+    }
+      
+    return $result;
+  }
+
+  public static function decode(string $number)
+  {
+    if(!preg_match('/[A-Z]/u',$number))return false;
+
+    $number = strrev($number);
+    $result = 0;
+
+    for($i=0; $i<strlen($number); $i++) {
+      $result += (26 ** $i) * (array_search($number[$i],self::$alphabet)+1);
+    }
+
+    return $result;
+  }
+}
