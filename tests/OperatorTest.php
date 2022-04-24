5afbd92ce1382bb56c240214e078cd4aa03aa385
Niki <82795329+NikiFutaki@users.noreply.github.com>
2022-04-24 15:03:00 +0900

    Add range

--- tests/OperatorTest.php
+++ tests/OperatorTest.php
@@ -10,6 +10,10 @@ class OperatorTest extends TestCase
     public function testIncrement()
     {
       $this->assertSame(false, Operator::increment(0));
+      $this->assertSame(false, Operator::decrement(""));
+      $this->assertSame(false, Operator::increment("a"));
+      $this->assertSame(false, Operator::increment("Test"));
+      $this->assertSame(false, Operator::increment("TEST007"));
       $this->assertSame('B', Operator::increment('A'));
       $this->assertSame('AA', Operator::increment('Z'));
       $this->assertSame('AB', Operator::increment('AA'));
@@ -17,11 +21,17 @@ class OperatorTest extends TestCase
       $this->assertSame('YDK', Operator::increment('YDJ'));
       $this->assertSame('GVQ', Operator::increment('GVP'));
       $this->assertSame('UOCMW', Operator::increment('UOCMV'));
+      $this->assertSame('FXSHRXW', Operator::increment('FXSHRXV'));
+      $this->assertSame(false, Operator::increment('FXSHRXW'));
     }
 
     public function testDecrement()
     {
       $this->assertSame(false, Operator::decrement(0));
+      $this->assertSame(false, Operator::decrement(""));
+      $this->assertSame(false, Operator::decrement("a"));
+      $this->assertSame(false, Operator::decrement("Test"));
+      $this->assertSame(false, Operator::decrement("TEST007"));
       $this->assertSame(false, Operator::decrement('A'));
       $this->assertSame('A', Operator::decrement('B'));
       $this->assertSame('Z', Operator::decrement('AA'));
@@ -30,5 +40,7 @@ class OperatorTest extends TestCase
       $this->assertSame('YDJ', Operator::decrement('YDK'));
       $this->assertSame('GVP', Operator::decrement('GVQ'));
       $this->assertSame('UOCMV', Operator::decrement('UOCMW'));
+      $this->assertSame('FXSHRXV', Operator::decrement('FXSHRXW'));
+      $this->assertSame(false, Operator::decrement('FXSHRXZ'));
     }
 }
